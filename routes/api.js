61a7d8fd1139b08d2f556b4a07332c7511111ed5
malwareami! <46420533+malicious-dev@users.noreply.github.com>
2022-08-09 03:15:41 +0530

    mail send on new users for reset password

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const db = "mongodb://127.0.0.1:27017/superadmin";
 const bcrypt = require('bcrypt');
 const checkAuth = require('../middleware/check-auth')
+const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 // const Crud = require('../models/crud')
@@ -182,7 +183,24 @@ else {
 
 })
 
+
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: 'kiera.homenick91@ethereal.email',
+    pass: 'h29EAZ5vmeDP2j5z1X'
+}
+});
+
 router.post('/add',checkAuth.auth,checkAuth.isverified, checkAuth.admin, (req, res) => {
+ 
+   
+   
+
+
   bcrypt.hash(req.body.password, 10 ,(err, hash) => {
     if (err) {
       return res.json({success: false, message:'hasing issue'})
@@ -206,69 +224,62 @@ router.post('/add',checkAuth.auth,checkAuth.isverified, checkAuth.admin, (req, r
         }
       })
 
-      console.log(req.body.name)
+      const payload = {
+        userId : user._id,
+      }
+      const token = jwt.sign(payload, process.env.TOKEN_KEY, {expiresIn: '300s'})
+    const resetPasswordUrl = `http://localhost:3000/resetPassword?token=`+token;
+      let info = transporter.sendMail({
+        from: '"Fred Foo 👻" kiera.homenick91@ethereal.email', // sender address
+        to: "kiera.homenick91@ethereal.email", // list of receivers
+        subject: "Reset Password ✔", // Subject line
+        text : `Dear user,
+      To reset your password, click on this link: ${resetPasswordUrl}
+      If you did not request any password resets, then ignore this email.`
+      }).then((data)=>{console.log('Mail sent', data)})
+      .catch(err => {console.error('Failure',err)})
     }
   })
   
 })
 
-//employye detail
+router.post('/resetPassword' , (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+console.log(token)
+  if (!token) {
+    return res.status(403).send("A token is to reset");
+  }
+  try {
+    const decoded = jwt.verify(token, 'herohamada');
+    console.log(decoded.userId)
+    bcrypt.hash(req.body.password, 10 ,(err, hash) => {
+      if (err) {
+        return res.json({success: false, message:'hasing issue'})
+      }else {
+            User.findById(decoded.userId,  (err, employee) => {
+          if (!employee)
+          return next(new Error('Unable To Find Employee With This Id'));
+          else {
+          employee.password = hash;
+      
+            
+          employee.save().then(emp => {
+          return res.json(employee);
+          })
+          .catch(err => {
+            // return res.json({success: false, message:'update issue'})
+            console.log('unable to update')
+          });
+          }
+          });
+      }})
+  
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+})
 
-// router.get('/allEmployee', (req, res) => {
-//   Crud.find((err, employees) => {
-//     if(err){
-//       console.log(err)
-//     }else {
-//       res.json(employees)
-//     }
-//   })
-// })
-
-// router.post('/addEmployee', (req, res) => {
-//   let employee = new Crud(req.body);
-//  employee.save()
-//  .then(game => {
-//  res.status(200).json({ 'employee': 'Employee Added Successfully' });
-//  })
-//  .catch(err => {
-//  res.status(400).send("Something Went Wrong");
-//  });
-// })
-
-// router.get('/editEmployee/:id', (req, res) => {
-//   let id = req.params.id;
-//   Crud.findById(id, function (err, employee) {
-//   res.json(employee);
-//   });
-// })
-
-// router.post('/updateEmployee/:id', (req, res) => {
-//   Crud.findById(req.params.id,  (err, employee) => {
-//     if (!employee)
-//     return next(new Error('Unable To Find Employee With This Id'));
-//     else {
-//     employee.firstName = req.body.firstName;
-//     employee.lastName = req.body.lastName;
-//     employee.email = req.body.email;
-//     employee.phone = req.body.phone;
-   
-//     employee.save().then(emp => {
-//     res.json('Employee Updated Successfully');
-//     })
-//     .catch(err => {
-//     res.status(400).send("Unable To Update Employee");
-//     });
-//     }
-//     });
-// })
-
-
-// router.get('/deleteEmployee/:id', (req, res) => {
-//   Crud.findByIdAndRemove({ _id: req.params.id }, function (err, employee) {
-//     if (err) res.json(err);
-//     else res.json('Employee Deleted Successfully');
-//     });
-// })
 
 
 module.exports = router;
